1924bbe4dc3ea84004e121e1de15513f690ec0ea
creightonja <creightonja@gmail.com>
2015-11-29 12:05:20 -0800

    Finish chapter 4 Lynda Java Essentials

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -2,7 +2,11 @@ package com.example.java;
 
 import java.math.BigDecimal;
 
+//Using primitive variable types
+
 public class Main {
+
+    static boolean bDef;
 
     public static void main(String[] args) {
 	    System.out.println("Hello from IDEA!");
@@ -10,6 +14,8 @@ public class Main {
         int myVar = 1;
         System.out.println("Your name is " + aString);
         System.out.println(myVar);
+
+
         byte b = 1;
         short sh = 1;
         int i = 1;
@@ -52,6 +58,83 @@ public class Main {
         double doubleValue = 3.9999999d;
         int intValue4 = (int) doubleValue;
         System.out.println(intValue4);
+
+        int intValue5 = 56;
+        int intValue6 = 42;
+
+        //Math library
+        int result1 = intValue5 + intValue6;
+        System.out.println("Addition: " + result1);
+
+        int result2 = intValue5 - intValue6;
+        System.out.println("Subtraction: " + result2);
+
+        int result3 = intValue5 * intValue6;
+        System.out.println("Mulitplying: " + result3);
+
+        double result4 = (double) intValue5 / intValue6;
+        System.out.println("Division: " + result4);
+
+        double result5 = (double) intValue5 % intValue6;
+        System.out.println("Remainder: " + result5);
+
+        double aDoubleValue = -3.99999999;
+        long rounded = Math.round(aDoubleValue);
+        System.out.println("Rounded: " + rounded);
+
+        double absValue = Math.abs(aDoubleValue);
+        System.out.println("Absolute: " + absValue);
+
+        boolean b1 = true;
+        boolean b2 = false;
+        boolean b3 = !b1;
+        int i1 = 0;
+        boolean b4 = (i1 != 0);
+
+        System.out.println("Value of b1 is: " + b1);
+        System.out.println("Value of b2 is: " + b2);
+        System.out.println("Value of b3 is: " + b3);
+        System.out.println("Value of b4 is: " + b4);
+        System.out.println("Default value of boolean bDef: " + bDef);
+
+        String sBoolean = "true";
+        boolean parsed = Boolean.parseBoolean(sBoolean);
+        System.out.println("Parsed: " + parsed);
+
+        //Single characters are a primitive data type, strings are not
+        char c1 = '1';
+        char c2 = '2';
+        char c3 = '3';
+        System.out.println("Char 1: " + c1);
+        System.out.println("Char 2: " + c2);
+        System.out.println("Char 3: " + c3);
+
+        char dollarSign = '\u0024';
+        System.out.println("Currency: " + dollarSign);
+
+        char a1 = 'a';
+        char a2 = 'b';
+        char a3 = 'c';
+
+        //Character library command
+        System.out.print(Character.toUpperCase(a1));
+        System.out.print(Character.toUpperCase(a2));
+        System.out.println(Character.toUpperCase(a3));
+
+        //Java string matching conditionals use special libary
+        String s1 = "Hello";
+        String s2 = "Hello";
+        if (s1.equals(s2)){
+            System.out.println("Match");
+        } else {
+            System.out.println("No Match");
+        }
+
+        //Ternary operator var = condition ? trueValue : falseValue
+        String isTrue = (b1) ? "It is True" : "It is False";
+        String isFalse = (!b1) ? "It is True" : "It is False";
+        System.out.println(isTrue);
+        System.out.println(isFalse);
 
     }
 }
