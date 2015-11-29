31cf7727ec5522fef7b0c86f1616ed725f946452
creightonja <creightonja@gmail.com>
2015-11-29 01:05:32 -0800

    first exercize files

---
+++ src/com/example/java/Main.java
@@ -0,0 +1,12 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello from IDEA!");
+        String aString = "Jason";
+        int myVar = 1;
+        System.out.println("Your name is " + aString);
+        System.out.println(myVar);
+    }
+}
